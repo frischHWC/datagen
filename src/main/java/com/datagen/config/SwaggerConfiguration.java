140b73cda0f49ecdc48128ad604622cbc33766d0
RISCH Francois <francois2.risch-prestataire@laposte.fr>
2024-06-03 07:39:54 +0200

    build(swagger): use new version of swagger

---
+++ src/main/java/com/datagen/config/SwaggerConfiguration.java
@@ -0,0 +1,41 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datagen.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfiguration {
+  @Bean
+  public OpenAPI createOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("Datagen API")
+            .description("Data Generation tool to multiple services")
+            .version("0.6.0")
+            .license(new License().name("Apache 2.0")))
+        .externalDocs(new ExternalDocumentation()
+            .description("Datagen Official Documentation")
+            .url("https://datagener.github.io/"));
+  }
+}
